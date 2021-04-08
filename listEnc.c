b4b9e5e1b8ec45b0a5abbdfc8a2fa8103f44ebb2
Matheus Madureira <matheus_mmadu@hotmail.com>
2021-04-08 20:52:46 -0300

    Projeto Final

--- listEnc.c
+++ listEnc.c
@@ -15,18 +15,43 @@ int main(){
     Aluno * proximo_aluno;
     int resp;
 
-    ini_aluno = malloc(sizeof(Aluno));
+    ini_aluno = (Aluno *)malloc(sizeof(Aluno));
     proximo_aluno = ini_aluno;
 
     while (1)
     {
         printf("\n");
+        fflush(stdin);
         printf("Digite o nome = ");
-        scanf("%s", &proximo_aluno->nome);
+        fgets(proximo_aluno->nome,30,stdin);
 
+        fflush(stdin);
+        printf("Digite a idade = ");
+        scanf("%i", &proximo_aluno->idade);
 
+        fflush(stdin);
+        printf("Deseja continuar? <1> SIM <2> NAO: ");
+        scanf("%i", &resp);
+
+        if ( resp == 1 ){
+            proximo_aluno->proximo = (Aluno *)malloc(sizeof(Aluno));
+            proximo_aluno = proximo_aluno->proximo;
+        }else{
+            break; // sair do loop
+        }
 
     }
+    proximo_aluno->proximo = NULL;
+    printf("\n");
 
+    proximo_aluno = ini_aluno;
+    printf("\n");
+
+    while(proximo_aluno != NULL)
+    {
+        printf("Nome: %sIdade: %i\n\n", proximo_aluno->nome, proximo_aluno->idade);
+        proximo_aluno = proximo_aluno->proximo;
+    } 
+    return 0;
 }
 
